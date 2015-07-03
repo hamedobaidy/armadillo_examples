7696ff00441b8dea93ac3eee0ae89b56fbd4a5c6
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-03 15:15:42 +0430

    Adding armadillo_element_init

---
+++ armadillo_element_init/src/armadillo_element_init.cpp
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : armadillo_element_init.cpp
+ Author      : Hamed Mohammadi
+ Version     :
+ Copyright   : GNU General Public License
+ Description : Hello World in C++,
+ ============================================================================
+ */
+
+#include <iostream>
+#include <armadillo>
+
+using namespace std;
+using namespace arma;
+
+int main(void) {
+	mat A;
+
+	A << 1 << 2 << 3 << endr
+	  << 4 << 5 << 6 << endr;
+	A.print("A:");
+
+	mat B = { 1, 2, 3, 4, 5, 6 };  // C++11 only
+	B.print("B:");
+	B.reshape(2,3);
+	B.print("B reshape:");
+	return 0;
+}
