f6828abf6c6310928547e971384ffc037bc5e206
hamedobaidy <hamed.obaidy@gmail.com>
2015-07-21 13:51:00 +0430

    edit

--- armadillo_examples/src/armadillo_example.cpp
+++ armadillo_examples/src/armadillo_example.cpp
@@ -16,7 +16,12 @@ using namespace arma;
 
 int main(int argc, char** argv) {
 	mat A = randu<mat>(4, 5);
+	A.print("A = ");
 	mat B = randu<mat>(4, 5);
+	B.print("B = ");
+
+	vec x = randu<vec>(3);
+	x.print("x =");
 
 	cout << A * B.t() << endl;
 
--- armadillo_examples/src/armadillo_examples.cpp
+++ armadillo_examples/src/armadillo_examples.cpp
@@ -7,9 +7,17 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
+#include <armadillo>
 
-int main() {
-	cout << "Hello World" << endl; // prints Hello World
-	return 0;
-}
+using namespace std;
+using namespace arma;
+
+int main(int argc, char** argv)
+  {
+  mat A = randu<mat>(4,5);
+  mat B = randu<mat>(4,5);
+
+  cout << A*B.t() << endl;
+
+  return 0;
+  }
